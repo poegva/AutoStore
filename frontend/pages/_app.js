5758e66e02cb0b670122b387d6f62d2976d02998
poegva <poegva@mail.ru>
2020-10-19 11:56:20 +0300

    SSR

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,19 +1,13 @@
 import React from "react";
 import Layout from "../components/Layout";
 import 'fontsource-roboto';
-import { useStore } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react'
 import { wrapper } from "../redux/store";
 
 function App({ Component, pageProps }) {
-    const store = useStore((state) => state);
-
     return (
-        <PersistGate loading={<div>loading...</div>} persistor={store.__persistor}>
-            <Layout>
-                <Component {...pageProps} />
-            </Layout>
-        </PersistGate>
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
     )
 }
 
